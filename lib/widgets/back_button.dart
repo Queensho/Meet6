b447f845459461124b7a6b3f8134eda4c52f019c
Queensho <65372013+Queensho@users.noreply.github.com>
2026-09-02 00:39:52 +0300

    Add reusable back button

---
+++ lib/widgets/back_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../theme/app_colors.dart';
+
+class Meet6BackButton extends StatelessWidget {
+  const Meet6BackButton({super.key, required this.onTap});
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(14),
+      child: Container(
+        width: 44,
+        height: 44,
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(.88),
+          borderRadius: BorderRadius.circular(14),
+          border: Border.all(color: AppColors.border),
+        ),
+        child: const Icon(
+          Icons.arrow_back_rounded,
+          color: AppColors.navy,
+        ),
+      ),
+    );
+  }
+}
