2b0b9cf07c398600e225c18fdb0eaaec353ba6e9
Queensho <65372013+Queensho@users.noreply.github.com>
2026-09-03 22:33:10 +0300

    Refresh app config from realtime admin broadcasts

--- lib/services/app_runtime_config_service.dart
+++ lib/services/app_runtime_config_service.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 
+import 'realtime_service.dart';
 import 'runtime_app_config_service.dart';
 
 typedef AppRuntimeConfig = RuntimeAppConfig;
@@ -7,9 +10,22 @@ typedef AppRuntimeConfig = RuntimeAppConfig;
 class AppRuntimeConfigService {
   const AppRuntimeConfigService._();
 
+  static StreamSubscription<RealtimeEvent>? _subscription;
+
   static ValueNotifier<RuntimeAppConfig> get value =>
       RuntimeAppConfigService.listenable;
 
-  static Future<RuntimeAppConfig> refresh() =>
-      RuntimeAppConfigService.load(force: true);
+  static void _ensureRealtime() {
+    if (_subscription != null) return;
+    _subscription = RealtimeService.events.listen((event) {
+      if (event.type == 'app:config') {
+        unawaited(RuntimeAppConfigService.load(force: true));
+      }
+    });
+  }
+
+  static Future<RuntimeAppConfig> refresh() {
+    _ensureRealtime();
+    return RuntimeAppConfigService.load(force: true);
+  }
 }
