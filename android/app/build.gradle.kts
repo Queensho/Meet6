bb5eac9f0d82fe92014a3f0582241ccaca6a763b
Queensho <65372013+Queensho@users.noreply.github.com>
2026-09-04 19:07:46 +0300

    Set Android minimum SDK for current RevenueCat Billing SDK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         applicationId = "com.meet6.app"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
