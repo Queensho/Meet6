478d34ddc2e05da2c875972282eaa41dba9d1e8a
Queensho <65372013+Queensho@users.noreply.github.com>
2026-09-04 17:20:57 +0300

    Clean observability imports

--- lib/services/observability_service.dart
+++ lib/services/observability_service.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
-import 'dart:ui';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ObservabilityService {
