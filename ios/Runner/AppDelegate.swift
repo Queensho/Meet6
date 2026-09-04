83ca77858adcf95abf41f1b8f15f4f115a643c8b
Queensho <65372013+Queensho@users.noreply.github.com>
2026-09-04 17:48:34 +0300

    Open iOS notification settings from Flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,8 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  private var systemNotificationChannel: FlutterMethodChannel?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -12,5 +14,27 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
+    let channel = FlutterMethodChannel(
+      name: "meet6/system_notifications",
+      binaryMessenger: engineBridge.applicationRegistrar.messenger()
+    )
+    channel.setMethodCallHandler { call, result in
+      guard call.method == "openSettings" else {
+        result(FlutterMethodNotImplemented)
+        return
+      }
+
+      guard let url = URL(string: UIApplication.openSettingsURLString),
+            UIApplication.shared.canOpenURL(url) else {
+        result(false)
+        return
+      }
+
+      UIApplication.shared.open(url, options: [:]) { opened in
+        result(opened)
+      }
+    }
+    systemNotificationChannel = channel
   }
 }
